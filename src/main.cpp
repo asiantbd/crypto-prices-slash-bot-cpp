3ae0a1421baf7f2b67232583b777083fd70bb67d
igprad <ign.aldi.pradana@gmail.com>
2023-10-11 15:35:01 +0700

    feat: add coins command (not tested yet)

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,36 @@ int main() {
   dpp::cluster bot(std::getenv("DISCORD_TOKEN"));
 
   bot.on_slashcommand([](auto event) {
-    if (event.command.get_command_name() == "ping") {
+    auto input = event.command.get_command_name();
+    if (input == "ping") {
       event.reply("Pong!");
     }
-
-    if (event.command.get_command_name() == "price") {
-      gecko::fetch(event);
+    if (input == "price") {
+      gecko::fetch_price(event);
     }
-
+    if (input == "coins") {
+      gecko::fetch_tokens(event);
+    }
     return 0;
   });
 
   bot.on_ready([&bot](auto event) {
     if (dpp::run_once<struct register_bot_commands>()) {
+      dpp::slashcommand command_coins;
+      command_coins.set_name("coins")
+          .set_description("List of available tokens from Coingecko.")
+          .set_application_id(bot.me.id);
+      bot.global_command_create(command_coins);
+
       dpp::slashcommand command_price;
       command_price.set_name("price")
-          .set_description("Get the price of a crypto token")
+          .set_description(
+              "Get the price of a crypto token. (Please use /coins to get all "
+              "listed tokens.)")
           .set_application_id(bot.me.id)
           .add_option(
               dpp::command_option(dpp::co_string, "coingecko_id",
                                   "(Coingecko) ID of the token: ", true));
-
       bot.global_command_create(command_price);
     }
   });

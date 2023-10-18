f8181026203d031371bcf5c14b7c75c3f31d62ea
igprad <ign.aldi.pradana@gmail.com>
2023-10-18 10:21:47 +0700

    fix: fix req body for quickchart api call and make the command only for 1 days

--- include/quickchart.h
+++ include/quickchart.h
@@ -8,7 +8,7 @@
 
 namespace qchart {
 
-std::string generate_chart(std::string label1, std::vector<long> data1,
-                           std::string label2, std::vector<long> data2);
+std::string generate_chart(std::vector<long> data1, std::string label,
+                           std::vector<double> data2);
 size_t write_callback(char* ptr, size_t size, size_t nmemb, std::string* data);
 }  // namespace qchart
